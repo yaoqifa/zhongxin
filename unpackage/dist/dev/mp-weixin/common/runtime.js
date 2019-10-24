b219211b4e14c3874dd304fbc355d6bc2a7feaac
yaoqifa <yaoqifa07330@hellobike.com>
2019-10-24 14:07:34 +0800

    tags

--- unpackage/dist/dev/mp-weixin/common/runtime.js
+++ unpackage/dist/dev/mp-weixin/common/runtime.js
@@ -104,11 +104,11 @@
 /******/
 /******/
 /******/ 		// mini-css-extract-plugin CSS loading
-/******/ 		var cssChunks = {"components/card":1};
+/******/ 		var cssChunks = {"components/card":1,"pages/tags/tag":1};
 /******/ 		if(installedCssChunks[chunkId]) promises.push(installedCssChunks[chunkId]);
 /******/ 		else if(installedCssChunks[chunkId] !== 0 && cssChunks[chunkId]) {
 /******/ 			promises.push(installedCssChunks[chunkId] = new Promise(function(resolve, reject) {
-/******/ 				var href = "" + ({"components/card":"components/card"}[chunkId]||chunkId) + ".wxss";
+/******/ 				var href = "" + ({"components/card":"components/card","pages/tags/tag":"pages/tags/tag"}[chunkId]||chunkId) + ".wxss";
 /******/ 				var fullhref = __webpack_require__.p + href;
 /******/ 				var existingLinkTags = document.getElementsByTagName("link");
 /******/ 				for(var i = 0; i < existingLinkTags.length; i++) {
